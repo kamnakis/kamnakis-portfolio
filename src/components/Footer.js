1dbf4d8d8620546bd9a1e1838ceb1c9ef7c5e3e4
Stylianos Kamnakis <kamnis1997@gmail.com>
2020-08-01 12:06:06 +0300

    Fix Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <div className="w-full flex items-center justify-center mb-10">
-      <div className="text-2xl text-black text-center">COPYRIGHTS © 2020. Designed by <a href="www.kamnakis.gr" className="text-secondary">kamankis</a></div>
+      <div className="text-2xl text-black text-center">COPYRIGHTS © 2020. Designed by <a href="https://kamnakis.herokuapp.com" target="_blank" className="text-secondary">kamankis</a></div>
     </div>
   );
 }
